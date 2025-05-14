9b84baf38238eacb56ff0a677e6bb5a0c2974289
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-05-13 22:07:23 -0400

    add nav menu open/close function with animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,21 @@ import Logo from "@/components/Logo";
 import MobileMenu from "@/components/nav/MobileMenu";
 import {useState} from "react";
 
+
+
 export default function Navbar() {
     const [isChecked, setIsChecked] = useState(false);
 
     const handleClick = () => {
         setIsChecked(prev => !prev);
     }
+
+    const asideClasses = `
+  bg-midnight text-beige p-4
+  ${isChecked ? "animate-slide-in" : "animate-slide-out"} 
+  mt-3 max-w-1/2 min-h-screen
+  lg:mt-0 lg:max-w-full lg:min-h-0 lg:static lg:translate-x-0 lg:animate-none lg:flex
+`;
 
 
     // flex justify-between items-center
@@ -21,15 +30,17 @@ export default function Navbar() {
             {/*</nav>*/}
 
             <div className="">
-                <label className="hamburger-menu cursor-pointer m-2 z-20">
+                <label className="hamburger-menu cursor-pointer m-2 z-20 lg:hidden">
                     <input id="menuCheckBox" type="checkbox" className="hidden peer" onChange={handleClick} />
                     <span className="hamburger-line peer-checked:rotate-45 peer-checked:translate-y-[8px]"/>
                     <span className="hamburger-line peer-checked:opacity-0 peer-checked:scale-x-0"/>
                     <span className="hamburger-line peer-checked:-rotate-45 peer-checked:-translate-y-[8px]"/>
                 </label>
 
-                <aside className={`bg-midnight text-beige mt-3 max-w-1/2 min-h-screen p-4 ${isChecked ? "animate-slide-in" : "animate-slide-out" }`}>
-                    <nav className="">
+
+
+                <aside className={asideClasses}>
+                    <nav className="flex flex-col gap-6 lg:flex-row lg:justify-end">
                       <label htmlFor="menuCheckBox">
                           <a href={"#about"} onClick={handleClick}>About</a>
                       </label>
