5eb5ea3dafffed8c35fa86182e339ddc7f261b09
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-05-19 20:56:47 -0400

    Change size of icon to 2rem

--- src/components/nav/NonMobileMenu.tsx
+++ src/components/nav/NonMobileMenu.tsx
@@ -4,14 +4,15 @@ import {DataSocialMedia} from "@/types/data.ts";
 export default function NonMobileMenu({data}: DataSocialMedia) {
     return (
             <aside className={`hidden lg:block`}>
-                <ul className="flex justify-center gap-6 xl:gap-7 w-full h-screen lg:h-fit ">
+                <ul className="flex justify-center gap-6 2xl:gap-7 w-full h-screen lg:h-fit ">
                     {data.map((media, index) => (
                         <li key={`${index}-${media.name}`}
                             className={"w-full flex justify-center"}>
                             <a href={media.link} target="_blank" rel="noopener noreferrer" draggable={false}
                                className={"flex items-center justify-center gap-0 active:text-orange-600"}>
-                                <img src={media.logo} alt={`${media.name.toLowerCase()}-logo`} draggable={false}
-                                     className={"w-6"}/>
+                                    <img src={media.logo} alt={`${media.name.toLowerCase()}-logo`} draggable={false}
+                                         className={"size-8"}/>
+
                             </a>
                         </li>
                     ))}
