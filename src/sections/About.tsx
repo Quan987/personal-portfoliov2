08953cea8635cf0c9b944dd277d0685bee45d812
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-05-20 20:25:20 -0400

    Refactor about me section for largest screen

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,25 +5,20 @@ import ParagraphText from "@/components/ui/ParagraphText.tsx";
 
 export default function About() {
     return (
-        <section id="about" className="py-35 scroll-m-12 border-1 border-black">
-            <div className="flex items-center justify-center gap-3">
-                <HorizontalLine lineWidth={"w-10"}/>
+        <section id="about" className="py-35 scroll-m-12">
+            <div className="flex flex-col items-center justify-center gap-3 py-15">
                 <h2 className="uppercase font-bold tracking-wider shrink-0 text-3xl">About Me</h2>
-                <HorizontalLine lineWidth={"w-10"}/>
+                <HorizontalLine lineWidth={"w-8"}/>
             </div>
 
-            <div className="flex flex-col justify-center items-center">
-                <div className="">
+            <div className="grid grid-cols-2 gap-20">
+                <div className="2xl:pl-30">
                     <ParagraphText text={data.aboutP1} className={"about-text"}/>
                     <ParagraphText text={data.aboutP2} className={"about-text"}/>
                     <ParagraphText text={data.aboutP3} className={"about-text"}>
-                        Let’s connect—follow me on <EmbeddedLink link={""} title="Linkedin"/> or <EmbeddedLink link={""}
-                                                                                                               title="Instagram"/> to
-                        stay in touch!
+                        Let’s connect—follow me on <EmbeddedLink link={""} title="Linkedin"/> or <EmbeddedLink link={""} title="Instagram"/> to stay in touch!
                     </ParagraphText>
                 </div>
-
-
                 <figure className="hover-scale w-fit">
                     <div className={"relative"}>
                         <img
@@ -31,11 +26,9 @@ export default function About() {
                             src={data.actionFigure}
                             alt="actionFigure"
                             draggable={false}
-                            className="object-contain cursor-pointer block w-xs 2xl:w-[25rem] rounded-[2rem] shadow-xl"
+                            className="inline-block object-contain cursor-pointer w-xs 2xl:w-[25rem] rounded-[2rem] shadow-xl"
                         />
-                        <p className={"tracking-wider absolute bottom-6 left-0 -rotate-90 origin-bottom-left"}>Limited
-                            Edition:
-                            Overthinking Included</p>
+                        <p className={"tracking-wider absolute bottom-6 right-0 rotate-90 origin-bottom-right text-black/60"}>Limited Edition: Overthinking Included</p>
                     </div>
                 </figure>
 
