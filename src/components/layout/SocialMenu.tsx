eb0158191e36d183d021a4d5b492d3ebf9563806
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-07-16 23:35:35 -0400

    Export SocialMenu into its own file

---
+++ src/components/layout/SocialMenu.tsx
@@ -0,0 +1,25 @@
+import { SOCIAL_LINKS } from "@/config/constants";
+
+export default function SocialMenu() {
+  return (
+    <ul className="flex items-center group gap-3">
+      {SOCIAL_LINKS.map((link, _) => {
+        // Calling the function to produce a react element, essentially this become: createElement(GitHubIcon)
+        const LinkIcon = link.icon;
+        return (
+          <li key={link.id} className="">
+            <a
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              draggable={false}
+              className="block size-full transition-colors duration-300 group-hover:text-secondary-muted hover:text-primary-dark px-2"
+            >
+              <LinkIcon className="2xl:size-6.5" />
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
