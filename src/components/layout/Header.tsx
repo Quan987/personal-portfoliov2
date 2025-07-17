4543bd725cc2d42018c21cd56ef5c87b749e1058
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-07-16 23:36:07 -0400

    Refactor NavMenu & Social Menu into their own file

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,55 +1,18 @@
-import { NavLink } from "react-router-dom";
-import { NAV_LINKS, SOCIAL_LINKS } from "@/config/constants";
+import NavMenu from "@/components/layout/NavMenu";
+import SocialMenu from "@/components/layout/SocialMenu";
 
 // isChecked ? "animate-slide-in" : "animate-slide-out"
 export default function Header() {
   return (
-    <header className={"absolute inset-x-0 top-0 2xl:py-5 2xl:px-10 z-100"}>
+    <header className={"z-header 2xl:py-5 2xl:px-15 2xl:text-lg"}>
       <nav className="flex justify-between items-center">
-        <div className={"font-medium text-black text-xl 2xl:pl-3.5"}>
-          Quan Tran
-        </div>
+        <div className={"font-medium text-primary-dark"}>Quan Tran</div>
 
         {/* Navigation internal */}
-        <ul className="flex items-center group">
-          {NAV_LINKS.map((link, index) => (
-            <li key={link.id} className="">
-              <NavLink
-                // to={`/${item === "Home" ? "" : item.toLowerCase()}`}
-                to={link.href}
-                className={({ isActive }) => `
-                inline-block h-fit w-full duration-300 group-hover:text-zinc-500 hover:text-black 2xl:px-3.5 ${
-                  isActive ? "text-black" : "text-zinc-500"
-                }`}
-                draggable={false}
-                end
-              >
-                {link.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
+        <NavMenu />
 
         {/* Navigation external */}
-        <ul className="flex items-center group">
-          {SOCIAL_LINKS.map((link, index) => {
-            // Calling the function to produce a react element, essentially this become: createElement(GitHubIcon)
-            const Icon = link.icon;
-            return (
-              <li key={link.id} className="">
-                <a
-                  href={link.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  draggable={false}
-                  className="block duration-300 group-hover:text-zinc-500 hover:text-black 2xl:px-3.5"
-                >
-                  <Icon />
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+        <SocialMenu />
       </nav>
     </header>
   );
