b70f57a359984e8cd94fd57d3d42f3c23bd70bdf
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-07-16 23:38:14 -0400

    Add rename file to export

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,3 +1,3 @@
 export * from "@/config/constants/app.constants";
-export * from "@/config/constants/nav.constants";
-export * from "@/config/constants/social.constants";
+export * from "@/config/constants/nav-links.constants";
+export * from "@/config/constants/social-links.constants";
