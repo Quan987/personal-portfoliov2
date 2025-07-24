2d5a48130bb53c79810828d675462e97ccc49426
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-07-23 22:26:15 -0400

    Export contact banner into its own file

---
+++ src/pages/ContactPage/components/ContactFormBanner.tsx
@@ -0,0 +1,16 @@
+export default function ContactFormBanner() {
+  return (
+    <div className={"text-primary-light flex flex-col items-center 2xl:p-25"}>
+      <p className={"text-3xl"}>Let's Connect</p>
+      <div className={"text-center"}>
+        <p className={"mt-10"}>
+          I'd love to hear from you — whether you have a question, want to
+          collaborate on a project, or just want to say hello. Please take a
+          moment to fill out the contact form, and I'll get back to you as soon
+          as I can.
+        </p>
+        <p className={"mt-10"}>Looking forward to connecting!</p>
+      </div>
+    </div>
+  );
+}
