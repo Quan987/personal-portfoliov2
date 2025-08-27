0e7c6530b0e3bdb6e3f6249d65bbc0ebc423f92c
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-08-26 22:48:32 -0400

    Move analytic & speed insight to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { routes } from "@routes/routes";
 import { RouterProvider } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <>
+      <RouterProvider router={routes} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -7,8 +7,6 @@ import Header from "@/components/layout/Header";
 import { useMemo } from "react";
 import { toMilliseconds } from "@/utils/time-utils";
 import { useRemoveSplashScreen } from "@/hooks/useRemoveSplashScreen";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function RootLayout() {
   useRemoveSplashScreen();
@@ -34,8 +32,6 @@ export default function RootLayout() {
         <Outlet />
       </main>
       <Footer />
-      <Analytics />
-      <SpeedInsights />
     </div>
   );
 }
