7a5d3d7877402a815863486be34a4f7f4b2d92cc
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-09-17 10:37:12 -0400

    Change lazy route to normal route mode

--- src/pages/contact-page/components/ui/FormFieldError.tsx
+++ src/pages/contact-page/components/ui/FormFieldError.tsx
@@ -6,7 +6,7 @@ type FormError = {
 
 export default function FormFieldError({ error }: FormError) {
   return (
-    <p className="text-error h-[clamp(1.25rem,1.65vw,2.25rem)] text-[clamp(0.65rem,0.75vw,1.125rem)]">
+    <p className="text-error h-[clamp(1.25rem,1.45vw,2rem)] text-[clamp(0.65rem,0.75vw,1.125rem)]">
       {error?.message}
     </p>
   );
