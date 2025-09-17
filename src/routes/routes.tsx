eeb0ccff4af56d0689993e3dc0c736854b3f19d1
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-09-17 12:41:07 -0400

    Refactor/animation (#6)
    * Remove useRouteLoadingIndicator usage for RouteLoadingIndicator
    
    * Move home constants to global constants folder & refactor root animation duration to theme layer
    
    * Refactor animation timing from layer root to layer theme
    
    * Change lazy route to normal route mode
    
    * Commented type lazy loading
    
    * Add lazy loading to route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,51 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "@/layouts/RootLayout.tsx";
 import ErrorPage from "@/pages/error-page";
-import Home from "@/pages/home-page";
-import About from "@/pages/about-page";
-import Project from "@/pages/project-page";
-import Contact from "@/pages/contact-page";
 
-// type LazyComponentPromise = Promise<() => JSX.Element>;
+import { JSX } from "react";
 
-// export const routes = createBrowserRouter([
-//   {
-//     path: "/",
-//     id: "root",
-//     errorElement: <ErrorPage />,
-//     element: <RootLayout />,
-//     children: [
-//       {
-//         index: true,
-//         lazy: {
-//           Component: async (): LazyComponentPromise =>
-//             (await import("@/pages/home-page")).default,
-//         },
-//       },
-//       {
-//         path: "about",
-//         lazy: {
-//           Component: async (): LazyComponentPromise =>
-//             (await import("@/pages/about-page")).default,
-//         },
-//       },
-//       {
-//         path: "projects",
-//         lazy: {
-//           Component: async (): LazyComponentPromise =>
-//             (await import("@/pages/project-page")).default,
-//         },
-//       },
-//       {
-//         path: "contact",
-//         lazy: {
-//           Component: async (): LazyComponentPromise =>
-//             (await import("@/pages/contact-page")).default,
-//         },
-//       },
-//     ],
-//   },
-// ]);
+type LazyComponentPromise = Promise<() => JSX.Element>;
 
 export const routes = createBrowserRouter([
   {
@@ -56,11 +15,50 @@ export const routes = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        lazy: {
+          Component: async (): LazyComponentPromise =>
+            (await import("@/pages/home-page")).default,
+        },
       },
-      { path: "about", element: <About /> },
-      { path: "projects", element: <Project /> },
-      { path: "contact", element: <Contact /> },
+      {
+        path: "about",
+        lazy: {
+          Component: async (): LazyComponentPromise =>
+            (await import("@/pages/about-page")).default,
+        },
+      },
+      {
+        path: "projects",
+        lazy: {
+          Component: async (): LazyComponentPromise =>
+            (await import("@/pages/project-page")).default,
+        },
+      },
+      {
+        path: "contact",
+        lazy: {
+          Component: async (): LazyComponentPromise =>
+            (await import("@/pages/contact-page")).default,
+        },
+      },
     ],
   },
 ]);
+
+// export const routes = createBrowserRouter([
+//   {
+//     path: "/",
+//     id: "root",
+//     errorElement: <ErrorPage />,
+//     element: <RootLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       { path: "about", element: <About /> },
+//       { path: "projects", element: <Project /> },
+//       { path: "contact", element: <Contact /> },
+//     ],
+//   },
+// ]);
