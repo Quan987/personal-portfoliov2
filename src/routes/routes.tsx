077098b0e040ee17d818a4a7f4ff8709941f48d5
Quan Tran <88959861+Quan987@users.noreply.github.com>
2025-09-17 10:37:12 -0400

    Change lazy route to normal route mode

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,8 +2,51 @@ import { JSX } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "@/layouts/RootLayout.tsx";
 import ErrorPage from "@/pages/error-page";
+import Home from "@/pages/home-page";
+import About from "@/pages/about-page";
+import Project from "@/pages/project-page";
+import Contact from "@/pages/contact-page";
 
 type LazyComponentPromise = Promise<() => JSX.Element>;
+
+// export const routes = createBrowserRouter([
+//   {
+//     path: "/",
+//     id: "root",
+//     errorElement: <ErrorPage />,
+//     element: <RootLayout />,
+//     children: [
+//       {
+//         index: true,
+//         lazy: {
+//           Component: async (): LazyComponentPromise =>
+//             (await import("@/pages/home-page")).default,
+//         },
+//       },
+//       {
+//         path: "about",
+//         lazy: {
+//           Component: async (): LazyComponentPromise =>
+//             (await import("@/pages/about-page")).default,
+//         },
+//       },
+//       {
+//         path: "projects",
+//         lazy: {
+//           Component: async (): LazyComponentPromise =>
+//             (await import("@/pages/project-page")).default,
+//         },
+//       },
+//       {
+//         path: "contact",
+//         lazy: {
+//           Component: async (): LazyComponentPromise =>
+//             (await import("@/pages/contact-page")).default,
+//         },
+//       },
+//     ],
+//   },
+// ]);
 
 export const routes = createBrowserRouter([
   {
@@ -14,32 +57,11 @@ export const routes = createBrowserRouter([
     children: [
       {
         index: true,
-        lazy: {
-          Component: async (): LazyComponentPromise =>
-            (await import("@/pages/home-page")).default,
-        },
+        element: <Home />,
       },
-      {
-        path: "about",
-        lazy: {
-          Component: async (): LazyComponentPromise =>
-            (await import("@/pages/about-page")).default,
-        },
-      },
-      {
-        path: "projects",
-        lazy: {
-          Component: async (): LazyComponentPromise =>
-            (await import("@/pages/project-page")).default,
-        },
-      },
-      {
-        path: "contact",
-        lazy: {
-          Component: async (): LazyComponentPromise =>
-            (await import("@/pages/contact-page")).default,
-        },
-      },
+      { path: "about", element: <About /> },
+      { path: "projects", element: <Project /> },
+      { path: "contact", element: <Contact /> },
     ],
   },
 ]);
